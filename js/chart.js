f6be68c56984f61b9d068870df9a93bcda7a6873
xffgithub <1025483506@qq.com>
2020-05-30 22:43:09 +0800

    '修改'

---
+++ js/chart.js
@@ -0,0 +1,32 @@
+// 基于准备好的dom，初始化echarts实例
+var myChart = echarts.init(document.getElementById("main"));
+// 指定图表的配置项和数据
+var option = {
+title: {
+    text: '业绩指标', //标题文本内容
+},
+toolbox: { //可视化的工具箱
+    show: true,
+    feature: {
+        restore: { //重置
+            show: true
+        },
+        saveAsImage: {//保存图片
+            show: true
+        }
+    }
+},
+tooltip: { //弹窗组件
+    formatter: "{a} <br/>{b} : {c}%"
+},
+series: [{
+    name: '业绩指标',
+    type: 'gauge',
+    detail: {formatter:'{value}%'},
+    data: [{value: 0, name: '完成率'}]
+}]
+
+};
+
+// 使用刚指定的配置项和数据显示图表。
+myChart.setOption(option);
